ed5610978c576b16fcb8777dc9c93d8d02e60fb0
JKamsker <jonas@kamsker.at>
2024-11-27 00:55:36 +0100

    Enhance ActionDisposable

---
+++ NYaul.Tests/ActionDisposableTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NYaul.Disposable;
+
+namespace NYaul.Tests;
+
+public class ActionDisposableTest
+{
+    [Fact]
+    public void ActionDisposable_Should_Reset_State()
+    {
+        var state = true;
+        {
+            using var _ = ActionDisposable.Create(false, true, ns => state = ns);
+            state.Should().BeFalse();
+        }
+        
+        state.Should().BeTrue();
+    }
+}
