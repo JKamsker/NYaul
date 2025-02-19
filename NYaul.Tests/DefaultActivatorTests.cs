0f1bfe21aebd1e0f5cdf94f7e4b49cd750f346c3
Jonas Kamsker <jonas.kamsker@suxxesso.com>
2025-02-19 19:44:29 +0100

    Implement DefaultActivator

---
+++ NYaul.Tests/DefaultActivatorTests.cs
@@ -0,0 +1,256 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using NYaul.Reflection;
+
+namespace NYaul.Tests;
+// Assuming DefaultActivator is in the DefaultActivatorNamespace
+
+public class DefaultActivatorTests
+{
+    #region Test Classes
+
+    // 1. Type with static CreateDefault method
+    public class TypeWithCreateDefault
+    {
+        public int Value { get; set; }
+
+        public static TypeWithCreateDefault CreateDefault()
+        {
+            return new TypeWithCreateDefault { Value = 10 };
+        }
+    }
+
+    // 2. Type with static Default property
+    public class TypeWithDefaultProperty
+    {
+        public int Value { get; set; }
+
+        public static TypeWithDefaultProperty Default => new TypeWithDefaultProperty { Value = 20 };
+    }
+
+    // 3. Type with static readonly Default field
+    public class TypeWithDefaultField
+    {
+        public int Value { get; set; }
+
+        public static readonly TypeWithDefaultField Default = new TypeWithDefaultField { Value = 30 };
+    }
+
+    // 4. Type with parameterless constructor
+    public class TypeWithParameterlessConstructor
+    {
+        public int Value { get; set; }
+
+        public TypeWithParameterlessConstructor()
+        {
+            Value = 40;
+        }
+    }
+
+    // 5. Type without parameterless constructor
+    public class TypeWithoutParameterlessConstructor
+    {
+        public int Value { get; set; }
+
+        public TypeWithoutParameterlessConstructor(int value)
+        {
+            Value = value;
+        }
+    }
+
+    // 6. Type with multiple default providers (CreateDefault and Default property)
+    public class TypeWithMultipleDefaults
+    {
+        public int Value { get; set; }
+
+        public static TypeWithMultipleDefaults CreateDefault()
+        {
+            return new TypeWithMultipleDefaults { Value = 50 };
+        }
+
+        public static TypeWithMultipleDefaults Default => new TypeWithMultipleDefaults { Value = 60 };
+    }
+
+    // 7. Type with incompatible CreateDefault method (wrong return type)
+    public class TypeWithIncompatibleCreateDefault
+    {
+        public int Value { get; set; }
+
+        public static string CreateDefault()
+        {
+            return "Not the correct type";
+        }
+    }
+
+    // 8. Type with no default mechanisms and no parameterless constructor
+    public class TypeWithNoDefaultMechanism
+    {
+        public int Value { get; set; }
+
+        private TypeWithNoDefaultMechanism()
+        {
+            Value = 70;
+        }
+
+        public TypeWithNoDefaultMechanism(int value)
+        {
+            Value = value;
+        }
+    }
+    
+    private void ActivateAndAssert<T>(Action<Func<T>> funcIn)
+    {
+        funcIn(DefaultActivator.CreateDefaultActivator<T>());
+        funcIn(() => DefaultActivator.CreateDefault<T>());
+    }
+
+
+    #endregion
+
+    #region Unit Tests
+
+    [Fact]
+    public void ActivateDefault_WithCreateDefaultMethod_ShouldUseCreateDefault()
+    {
+        ActivateAndAssert<TypeWithCreateDefault>(func =>
+        {
+            // Act
+            var instance = func();
+
+            // Assert
+            instance.Should().NotBeNull();
+            instance.Value.Should().Be(10);
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_WithDefaultProperty_ShouldUseDefaultProperty()
+    {
+        ActivateAndAssert<TypeWithDefaultProperty>(func =>
+        {
+            // Act
+            var instance = func();
+
+            // Assert
+            instance.Should().NotBeNull();
+            instance.Value.Should().Be(20);
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_WithDefaultField_ShouldUseDefaultField()
+    {
+        ActivateAndAssert<TypeWithDefaultField>(func =>
+        {
+            // Act
+            var instance = func();
+
+            // Assert
+            instance.Should().NotBeNull();
+            instance.Value.Should().Be(30);
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_WithParameterlessConstructor_ShouldUseConstructor()
+    {
+        ActivateAndAssert<TypeWithParameterlessConstructor>(func =>
+        {
+            // Act
+            var instance = func();
+
+            // Assert
+            instance.Should().NotBeNull();
+            instance.Value.Should().Be(40);
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_WithoutParameterlessConstructor_ShouldUseActivator()
+    {
+        ActivateAndAssert<TypeWithoutParameterlessConstructor>(func =>
+        {
+            // Act
+            Action act = () => func();
+
+            // Assert
+            act.Should().Throw<MissingMethodException>();
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_WithMultipleDefaults_ShouldPrioritizeCreateDefault()
+    {
+        ActivateAndAssert<TypeWithMultipleDefaults>(func =>
+        {
+            // Act
+            var instance = func();
+
+            // Assert
+            instance.Should().NotBeNull();
+            instance.Value.Should().Be(50); // Should use CreateDefault, not Default property
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_WithIncompatibleCreateDefault_ShouldFallback()
+    {
+        ActivateAndAssert<TypeWithIncompatibleCreateDefault>(func =>
+        {
+            // Act
+            var instance = func();
+
+            // Assert
+            // Since CreateDefault has incompatible return type, it should fallback to parameterless constructor
+            instance.Should().NotBeNull();
+            instance.Value.Should().Be(0); // Default int value
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_WithNoDefaultMechanismAndNoParameterlessConstructor_ShouldUseActivator()
+    {
+        ActivateAndAssert<TypeWithNoDefaultMechanism>(func =>
+        {
+            // Act
+            Action act = () => func();
+
+            // Assert
+            // Since there's no parameterless constructor, Activator.CreateInstance<T>() should be used
+            // However, Activator.CreateInstance<T>() requires a parameterless constructor, so it should throw.
+            act.Should().Throw<MissingMethodException>();
+        });
+    }
+
+    [Fact]
+    public void ActivateDefault_ShouldCacheDelegates()
+    {
+        // Arrange
+        var func1 = DefaultActivator.CreateDefaultActivator<TypeWithCreateDefault>();
+        var func2 = DefaultActivator.CreateDefaultActivator<TypeWithCreateDefault>();
+        
+        // Act & Assert
+        func1.Should().BeSameAs(func2); // The cached delegate should be the same instance
+    }
+
+    [Fact]
+    public void ActivateDefault_ShouldReturnDifferentInstances()
+    {
+        // Arrange
+        ActivateAndAssert<TypeWithCreateDefault>(func =>
+        {
+            // Act
+            var instance1 = func();
+            var instance2 = func();
+
+            // Assert
+            instance1.Should().NotBeSameAs(instance2);
+            instance1.Value.Should().Be(10);
+            instance2.Value.Should().Be(10);
+        });
+
+    }
+
+   
+    #endregion
+}
---
+++ NYaul/Reflection/DefaultActivator.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
+
+namespace NYaul.Reflection
+{
+    /// <summary>
+    /// Provides methods to create default instances of types using optimized activators.
+    /// </summary>
+    public static class DefaultActivator
+    {
+        /// <summary>
+        /// Cache to store the compiled delegates for each type <typeparamref name="T"/>.
+        /// </summary>
+        private static readonly ConcurrentDictionary<Type, object> _defaultFuncs = new ConcurrentDictionary<Type, object>();
+
+        /// <summary>
+        /// Retrieves the optimized <see cref="Func{T}"/> to create a default instance of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type for which to create the default instance.</typeparam>
+        /// <returns>A <see cref="Func{T}"/> delegate that creates the default instance.</returns>
+        public static Func<T> CreateDefaultActivator<T>()
+        {
+            // Try to get the cached delegate
+            if (_defaultFuncs.TryGetValue(typeof(T), out var cachedFunc))
+            {
+                return (Func<T>)cachedFunc;
+            }
+
+            // Build the delegate
+            var func = BuildActivateDefaultFunc<T>();
+
+            // Cache the delegate for future use
+            _defaultFuncs[typeof(T)] = func;
+
+            return func;
+        }
+
+        /// <summary>
+        /// Creates a default instance of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to instantiate.</typeparam>
+        /// <returns>A default instance of type <typeparamref name="T"/>.</returns>
+        /// <exception cref="MissingMethodException">
+        /// Thrown if no suitable constructor, method, property, or field is found to create an instance.
+        /// </exception>
+        public static T CreateDefault<T>()
+        {
+            var type = typeof(T);
+            var context = new TypeContext(type);
+
+            // 1. Try to use CreateDefault method
+            var createDefaultMethod = context.CreateDefaultMethod;
+            if (createDefaultMethod != null && type.IsAssignableFrom(createDefaultMethod.ReturnType))
+            {
+                return (T)createDefaultMethod.Invoke(null, null);
+            }
+
+            // 2. Try to use Default property
+            var defaultProperty = context.DefaultProperty;
+            if (defaultProperty != null && type.IsAssignableFrom(defaultProperty.PropertyType))
+            {
+                return (T)defaultProperty.GetValue(null);
+            }
+
+            // 3. Try to use Default field
+            var defaultField = context.DefaultField;
+            if (defaultField != null && type.IsAssignableFrom(defaultField.FieldType))
+            {
+                return (T)defaultField.GetValue(null);
+            }
+
+            // 4. Fallback to parameterless constructor or Activator.CreateInstance<T>()
+            var parameterlessConstructor = context.ParameterlessConstructor;
+            if (parameterlessConstructor != null)
+            {
+                return (T)parameterlessConstructor.Invoke(null);
+            }
+
+            return Activator.CreateInstance<T>();
+        }
+
+        /// <summary>
+        /// Builds the optimized <see cref="Func{T}"/> delegate based on the <see cref="TypeContext"/>.
+        /// </summary>
+        /// <typeparam name="T">The type for which to build the delegate.</typeparam>
+        /// <returns>A compiled <see cref="Func{T}"/> delegate.</returns>
+        private static Func<T> BuildActivateDefaultFunc<T>()
+        {
+            var type = typeof(T);
+            var context = new TypeContext(type);
+
+            // 1. Try to use CreateDefault method
+            var createDefaultMethod = context.CreateDefaultMethod;
+            if (createDefaultMethod != null && type.IsAssignableFrom(createDefaultMethod.ReturnType))
+            {
+                // Create a delegate that invokes the static method
+                var methodCall = Expression.Call(createDefaultMethod);
+                return Expression.Lambda<Func<T>>(methodCall).Compile();
+            }
+
+            // 2. Try to use Default property
+            var defaultProperty = context.DefaultProperty;
+            if (defaultProperty != null && type.IsAssignableFrom(defaultProperty.PropertyType))
+            {
+                // Create a delegate that accesses the static property
+                var propertyAccess = Expression.Property(null, defaultProperty);
+                return Expression.Lambda<Func<T>>(propertyAccess).Compile();
+            }
+
+            // 3. Try to use Default field
+            var defaultField = context.DefaultField;
+            if (defaultField != null && type.IsAssignableFrom(defaultField.FieldType))
+            {
+                // Create a delegate that accesses the static field
+                var fieldAccess = Expression.Field(null, defaultField);
+                return Expression.Lambda<Func<T>>(fieldAccess).Compile();
+            }
+
+            // 4. Fallback to parameterless constructor or Activator.CreateInstance<T>()
+            var parameterlessConstructor = context.ParameterlessConstructor;
+            if (parameterlessConstructor != null)
+            {
+                var newExpression = Expression.New(parameterlessConstructor);
+                return Expression.Lambda<Func<T>>(newExpression).Compile();
+            }
+
+            return () => Activator.CreateInstance<T>();
+        }
+
+        /// <summary>
+        /// Encapsulates reflection information for a specific type using lazy initialization.
+        /// </summary>
+        private class TypeContext
+        {
+            private readonly Type _type;
+
+            // Lazy backing fields for reflection members
+            private readonly Lazy<MethodInfo?> _createDefaultMethod;
+            private readonly Lazy<PropertyInfo?> _defaultProperty;
+            private readonly Lazy<FieldInfo?> _defaultField;
+            private readonly Lazy<ConstructorInfo?> _parameterlessConstructor;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="TypeContext"/> class for the specified type.
+            /// </summary>
+            /// <param name="type">The type to analyze.</param>
+            /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
+            public TypeContext(Type type)
+            {
+                _type = type ?? throw new ArgumentNullException(nameof(type));
+
+                // Initialize Lazy fields with thread-safe publication
+                _createDefaultMethod = new Lazy<MethodInfo?>(() =>
+                        _type.GetMethod("CreateDefault", BindingFlags.Static | BindingFlags.Public, null, Type.EmptyTypes, null),
+                    LazyThreadSafetyMode.ExecutionAndPublication);
+
+                _defaultProperty = new Lazy<PropertyInfo?>(() =>
+                        _type.GetProperty("Default", BindingFlags.Static | BindingFlags.Public),
+                    LazyThreadSafetyMode.ExecutionAndPublication);
+
+                _defaultField = new Lazy<FieldInfo?>(() =>
+                        _type.GetField("Default", BindingFlags.Static | BindingFlags.Public),
+                    LazyThreadSafetyMode.ExecutionAndPublication);
+
+                _parameterlessConstructor = new Lazy<ConstructorInfo?>(() =>
+                        _type.GetConstructor(Type.EmptyTypes),
+                    LazyThreadSafetyMode.ExecutionAndPublication);
+            }
+
+            /// <summary>
+            /// Gets the <see cref="MethodInfo"/> for the static <c>CreateDefault</c> method, if it exists.
+            /// </summary>
+            public MethodInfo? CreateDefaultMethod => _createDefaultMethod.Value;
+
+            /// <summary>
+            /// Gets the <see cref="PropertyInfo"/> for the static <c>Default</c> property, if it exists.
+            /// </summary>
+            public PropertyInfo? DefaultProperty => _defaultProperty.Value;
+
+            /// <summary>
+            /// Gets the <see cref="FieldInfo"/> for the static <c>Default</c> field, if it exists.
+            /// </summary>
+            public FieldInfo? DefaultField => _defaultField.Value;
+
+            /// <summary>
+            /// Gets the <see cref="ConstructorInfo"/> for the parameterless constructor, if it exists.
+            /// </summary>
+            public ConstructorInfo? ParameterlessConstructor => _parameterlessConstructor.Value;
+        }
+    }
+}
